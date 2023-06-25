ff093cd21971e72695fa54613e58fca0974c3058
liudongxue <1096074393@qq.com>
2023-06-25 13:27:14 +0800

    element-plus配置

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,11 @@
 
 import "../element-variables.scss";
 import ElementPlus from "element-plus";
-import zhcn from "element-plus/es/locale/lang/zh-cn";
+//import zhcn from "element-plus/es/locale/lang/zh-cn";
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 export default (app) => {
-  app.use(ElementPlus, { locale: zhcn });
+  app.use(ElementPlus);
   for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
   }
